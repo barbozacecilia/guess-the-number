3bb06e332827764f66bf8dea82e5873ea0f7f0a2
barbozacecilia <barboza.ceciliam@gmail.com>
2022-06-09 19:12:35 -0300

    (Clue): Add some clues with *ngFor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,17 +9,56 @@ export class HomeComponent implements OnInit {
   number:any ="";
   title:any='';
   modify:any=false;
+  tamano:number=2;
+  cssTitle:string=""
 
+  op1:number=0
+  op2:number=0
+ result:any
+
+ cssClassResult:string=""
+  idea:any=[
+    {
+      id:1,
+      name:"I was born that day"
+    },
+    {
+      id:2,
+      name:"If you find it in a horizontal position, it seems infinite"
+    }
+  ]
   constructor() { }
   changeTitle(){
     if(this.number===8){
       this.title="You Win!"
+      this.cssTitle="red"
     }
-    else{
-      this.title="You lose!"
+    else{(this.number!==8)
+      this.title="You lost your chance"
     }
     this.modify=true
+
   }
+  sum(){
+    this.result= this.op1 +this.op2
+    if(this.result>0){
+      this.cssClassResult="red"
+    }else if
+      (this.result>0){
+      this.cssClassResult="blue"
+    }else{
+      this.cssClassResult=""
+    }
+  }
+  changeProduct(){
+  this.idea=[
+    {
+    id:3,
+    name:"I'm not going to tell you"
+    }
+  ]
+}
+
 
   ngOnInit(): void {
   }
